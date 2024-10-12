5fc0279619812fecf6f623b1427489f0888fe411
fouadkhalied <foukha49@gmail.com>
2024-10-12 18:34:30 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const router = require('./api');
+
+app.use(router);
+app.use(express.json());
+
+const port = process.env.PORT || 5000
+
+app.listen(port,()=>{console.log('app run on port 5000')})
+
+
+
