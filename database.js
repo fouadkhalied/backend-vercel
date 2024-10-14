f3befd2a9ea20c9324682732adc6600c0bd37910
fouadkhalied <foukha49@gmail.com>
2024-10-14 06:31:06 +0300

    test7

--- database.js
+++ database.js
@@ -1,14 +1,2 @@
 const sql = require('mssql/msnodesqlv8');
 
-const config = {
-    server : "test",
-    database : "store",
-    driver : "{SQL Server}",
-    options : {
-        TrustedConnection : true
-    } 
-}
-
-sql.connect(config , (err)=>{
-    console.log(err)
-})
